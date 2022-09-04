c40207b9259dfdf62610ce7fa7f5137f14443927
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-04 11:08:05 -0300

    chore: Test forms submit on Seacrch component.

--- app/frontend/src/components/Search.jsx
+++ app/frontend/src/components/Search.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from './Select';
 // import eventTypes from '../services/eventTypes';
 
 export default function Search() {
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState('teste');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { target } = e;
+    console.log(target.register.value);
+    setShowMessage(true);
+    setMessage('foi');
+  };
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center ">
       <h2 className="page-title">Busca e Cadastro</h2>
-      <form className="w-full max-w-lg flex flex-wrap mx-3 mb-6">
+      <form
+        className="w-full max-w-lg flex flex-wrap mx-3 mb-6"
+        onSubmit={ handleSubmit }
+      >
         <div className="w-full px-3 mb-6 md:mb-0">
           <label
             className="
             label-form"
-            htmlFor="grid-first-name"
+            htmlFor="form-name"
           >
             Nome
             <input
+              name="name"
               className="input-form"
-              id="grid-first-name"
+              id="form-name"
               type="text"
               placeholder="Nome"
             />
@@ -26,12 +39,12 @@ export default function Search() {
           <label
             className="
               label-form"
-            htmlFor="grid-last-name"
+            htmlFor="form-email"
           >
             E-mail
             <input
               className="input-form"
-              id="grid-last-name"
+              id="form-email"
               type="text"
               placeholder="exemplo@exemplo.com"
             />
@@ -39,14 +52,13 @@ export default function Search() {
         </div>
         <div className="w-full  px-3">
           <label
-            className="
-              label-form"
-            htmlFor="grid-last-name"
+            className="label-form"
+            htmlFor="form-cpf"
           >
             CPF
             <input
               className="input-form"
-              id="grid-last-name"
+              id="form-cpf"
               type="text"
               placeholder="Apenas números"
             />
@@ -56,57 +68,45 @@ export default function Search() {
           <label
             className="
               label-form"
-            htmlFor="grid-last-name"
+            htmlFor="formt-latitude"
           >
             (Localização) Latitude
             <input
               className="input-form"
-              id="grid-last-name"
+              id="formt-latitude"
               type="text"
               placeholder="Apenas números"
             />
           </label>
         </div>
         <div className="w-1/2  px-3">
-          <label
-            className="
-              label-form"
-            htmlFor="grid-last-name"
-          >
+          <label className="label-form" htmlFor="formt-longitude">
             (Localização) Longitude
             <input
               className="input-form"
-              id="grid-last-name"
+              id="formt-longitude"
               type="text"
               placeholder="Apenas números"
             />
           </label>
         </div>
         <div className="w-1/2  px-3">
-          <label
-            className="
-              label-form"
-            htmlFor="grid-last-name"
-          >
+          <label className="label-form" htmlFor="form-type">
             Tipo da Lavoura
             <input
               className="input-form"
-              id="grid-last-name"
+              id="form-type"
               type="text"
               placeholder="milho, soja, feijão, ..."
             />
           </label>
         </div>
         <div className="w-1/2  px-3">
-          <label
-            className="
-              label-form"
-            htmlFor="grid-last-name"
-          >
+          <label className="label-form" htmlFor="form-date">
             Data da Colheita
             <input
               className="input-form"
-              id="grid-last-name"
+              id="form-date"
               type="date"
               placeholder="Apenas números"
             />
@@ -114,10 +114,25 @@ export default function Search() {
         </div>
         <Select />
         <div className="w-full flex justify-around px-3 mt-3 mb-6 md:mb-0">
-          <button className="w-1/3 button-form" type="button">Cadastrar</button>
-          <button className="w-1/3 button-form" type="button">Atualizar</button>
+          <button
+            className="w-1/3 button-form"
+            type="submit"
+            value="register"
+            name="register"
+          >
+            Cadastrar
+          </button>
+          <button
+            className="w-1/3 button-form"
+            type="submit"
+            value="update"
+            name="update"
+          >
+            Atualizar
+          </button>
         </div>
       </form>
+      {showMessage && <p>{message}</p>}
     </div>
   );
 }
