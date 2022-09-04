e296ae77a2d4c8953c73c39fb80813483c72257e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-04 15:06:09 -0300

    fix: Fix validateCPF function.
    Variables were not defined.

--- app/frontend/src/services/validateCPF.js
+++ app/frontend/src/services/validateCPF.js
@@ -1,33 +1,47 @@
 // reference: https://www.devmedia.com.br/validar-cpf-com-javascript/23916
 function validateCPF(cpf) {
-  if (cpf === '00000000000') return false;
-
-  let sum;
-  let rest;
-  sum = 0;
-
   const nine = 9;
   const ten = 10;
   const eleven = 11;
   const twelve = 12;
+  const minus = -1;
 
-  for (i = 1; i <= nine; i += 1) {
-    sum += parseInt(cpf.substring(i - 1, i), 10) * (eleven - i);
+  const strCPF = String(cpf).replace(/[^\d]/g, '');
+  if (strCPF.length !== eleven) return false;
+  if ([
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+  ].indexOf(strCPF) !== minus) { return false; }
+
+  let sum = 0;
+  let rest;
+
+  for (let i = 1; i <= nine; i += 1) {
+    sum += parseInt(strCPF.substring(i - 1, i), 10) * (eleven - i);
   }
   rest = (sum * ten) % eleven;
 
   // Validate last digits
   if ((rest === ten) || (rest === eleven)) rest = 0;
-  if (rest !== parseInt(cpf.substring(nine, ten), 10)) return false;
+  if (rest !== parseInt(strCPF.substring(nine, ten), 10)) return false;
 
   sum = 0;
-  for (i = 1; i <= ten; i += 1) {
-    sum += parseInt(cpf.substring(i - 1, i), 10) * (twelve - i);
+  for (let i = 1; i <= ten; i += 1) {
+    sum += parseInt(strCPF.substring(i - 1, i), 10) * (twelve - i);
   }
   rest = (sum * ten) % eleven;
 
   if ((rest === ten) || (rest === eleven)) rest = 0;
-  return (rest === parseInt(cpf.substring(ten, eleven), 10));
+
+  return rest === parseInt(strCPF.substring(ten, eleven), 10);
 }
 
 export default validateCPF;
