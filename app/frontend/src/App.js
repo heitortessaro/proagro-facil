56ac8e12b9eadc4ee8fa292390c9772fe8ada554
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-04 15:41:46 -0300

    refactor: Change form multi page to single page.
    Now a select is used so select the service.

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,14 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import Search from './components/Search';
+import Register from './components/Register';
 
 export default function App() {
+  const [service, setService] = useState('register');
+  const selectService = ({ target }) => {
+    console.log(target.value);
+    setService('register');
+  };
+  console.log(service);
   return (
-    <div>
+    <div className="w-full h-screen max-w-[1000px] mx-auto">
       <Navbar />
-      <Routes>
-        <Route path="/" element={ <Search /> } />
-      </Routes>
+      <div className="my-8">
+        <label className="label-main" htmlFor="service">
+          Serviço Desejado
+          <select
+            onClick={ selectService }
+            className="input-form"
+            id="service"
+            name="service"
+          >
+            <option value="register">Registrar Ocorrência</option>
+            <option value="update">Atualizar Registro</option>
+            <option value="search">Buscar Registro</option>
+            <option value="delete">Apagar Registro</option>
+          </select>
+        </label>
+      </div>
+      {service === 'register' && <Register />}
     </div>
   );
 }
