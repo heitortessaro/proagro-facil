ee061fa3bc7d456212702d1f7d74b82c477edb8c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-05 18:53:44 -0300

    ci: Add pydantic and create schemas for proagro.

---
+++ app/backend/app/schemas/proagro.py
@@ -0,0 +1,25 @@
+from datetime import date
+from pydantic import BaseModel, EmailStr, Field
+
+
+class StudentSchema(BaseModel):
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    cpf: str = Field(...)
+    latitude: float = Field(...)
+    longitude: float = Field(...)
+    type: str = Field(...)
+    date: date = Field(...)
+    event: str = Field(...)
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
