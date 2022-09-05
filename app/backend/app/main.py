ce2c746b071388830224852bb3c0cb0fb0f787f4
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-05 19:28:16 -0300

    feat: Include the register route to the main.

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
-from app.api import ping
+from app.routes.register import router as RegisterRouter
+from app.routes import ping
 
 app = FastAPI()
 
+app.include_router(RegisterRouter, tags=["Register"], prefix="/register")
 app.include_router(ping.router)
 
 
