94d14635da4c261ae4e9275beb4e1783a551112e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 15:04:34 -0300

    fix: Fix the RegisterSchema.
    Add a config to the class with an example.

--- app/backend/app/schemas/proagro.py
+++ app/backend/app/schemas/proagro.py
@@ -1,4 +1,3 @@
-from datetime import date
 from pydantic import BaseModel, EmailStr, Field
 
 
@@ -9,9 +8,22 @@ class RegisterSchema(BaseModel):
     latitude: float = Field(...)
     longitude: float = Field(...)
     type: str = Field(...)
-    date: date = Field(...)
+    date: str = Field(...)
     event: str = Field(...)
 
+    class Config:
+            schema_extra = {
+                "example": {  
+                    "name": "teste4",
+                    "email": "teste@teste.com",
+                    "cpf": "03214598678",
+                    "latitude": "56",
+                    "longitude": "55",
+                    "type": "milho",
+                    "date": "2022-10-25",
+                    "event": "granizo"
+                }
+            }
 
 def ResponseModel(data, message):
     return {
