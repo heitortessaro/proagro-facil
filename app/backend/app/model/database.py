3e652dcd17147c6f5936903ddf7c0a7989ed141c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 15:02:02 -0300

    fix: Replace the db with MongoDb Atlas.
    The access information is hard coded. It should be replaced in the future.

--- app/backend/app/model/database.py
+++ app/backend/app/model/database.py
@@ -2,10 +2,12 @@ import motor.motor_asyncio
 from bson.objectid import ObjectId
 
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+# MONGO_DETAILS = "mongodb://localhost:27017"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+# client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://mongo-heitor:root@cluster0.2eyqb11.mongodb.net/?retryWrites=true&w=majority")
 
 database = client.proagro
 
-proagro_collection = database.get_collection("proagro_collection")
+proagro_collection = database.get_collection('proagro_collection')
