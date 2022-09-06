b5af2b8381b0d591af437955ae356f4d33e90172
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 15:10:27 -0300

    feat: Create route to fetch register by id and fix post route.
    Add description for both routes

--- app/backend/app/routes/register.py
+++ app/backend/app/routes/register.py
@@ -1,25 +1,39 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, HTTPException
 from fastapi.encoders import jsonable_encoder
 
 from app.model.proagro import (
-  add_register
+  add_register,
+  fetch_one_register,
 )
 
 from app.schemas.proagro import (
   ErrorResponseModel,
   ResponseModel,
-  RegisterSchema
+  RegisterSchema,
 )
 
 router = APIRouter()
 
-@router.post("/", response_description="Adiciona um novo registro de evento à base de dados")
-async def add_register_data(register: RegisterSchema = Body(...)):
+@router.post(
+  "/",
+  status_code=201,
+  response_description="Adiciona um novo registro de evento à base de dados",
+  )
+async def add_register_data(register:RegisterSchema = Body(...)):
     register = jsonable_encoder(register)
     new_register = await add_register(register)
-    return ResponseModel(new_register, "Registro de evento cadastrado com sucesso.")
+    return new_register
 
-
+@router.get(
+  "/{id}",
+  status_code=200,
+  response_description="Retorna o registro de evento com o id fornecido",
+  )
+async def get_by_id(id):
+    response = await fetch_one_register(id)
+    if response:
+        return response 
+    raise HTTPException(404, f"Não existe registro de evento com o id {id}")
 
 
 @router.get("/ping")
