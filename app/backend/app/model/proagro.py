d0d6ef73cfef00ddd682501483641ff634c5ca20
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 20:39:39 -0300

    feat: Add new features to the model proagro.
    Function to update_register, delete_register and fetch_register_by_date were created.

--- app/backend/app/model/proagro.py
+++ app/backend/app/model/proagro.py
@@ -1,5 +1,7 @@
+from fastapi import HTTPException
 from app.model.database import proagro_collection
 from bson.objectid import ObjectId
+from typing import List
 
 # helpers
 def register_helper(register) -> dict:
@@ -22,14 +24,43 @@ async def add_register(register_data: dict) -> dict:
     return register_helper(new_register)
 
 # find a register using id
-async def fetch_one_register(id):
+async def fetch_one_register(id: str) -> dict:
   register = await proagro_collection.find_one({"_id": ObjectId(id)})
   return register_helper(register)
 
+# find registers using date
+async def fetch_registers_by_date(date: str) -> List[dict]:
+  registers = []
+  async for register in proagro_collection.find({"date": date}):
+    registers.append(register_helper(register))
+  return registers
+
 # find all registers
-async def fetch_all():
+async def fetch_all() -> List[dict]:
   registers = []
   async for register in proagro_collection.find():
     registers.append(register_helper(register))
   return registers
 
+# delete a register
+async def delete_register(id: str):
+    register = await proagro_collection.find_one({"_id": ObjectId(id)})
+    if register:
+        await proagro_collection.delete_one({"_id": ObjectId(id)})
+        return True
+    return False
+
+# update a register with matching id
+async def update_register(id: str, data: dict) -> dict:
+    # Return false if an empty request body is sent.
+    if len(data) < 1:
+        raise HTTPException(400, "Não foram encaminhados os dados atualizados do evento.")
+    register = await proagro_collection.find_one({"_id": ObjectId(id)})
+    if register:
+        updated_register = await proagro_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+        if updated_register:
+            return True
+        return False
+    return False
