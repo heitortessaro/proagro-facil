ebae425af396ce55cac38a4a18c9cd032c1c22fb
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 15:11:29 -0300

    ci: Add middleware with Cors to the main.

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.register import router as RegisterRouter
 from app.routes import ping
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(RegisterRouter, tags=["Register"], prefix="/register")
 app.include_router(ping.router)
