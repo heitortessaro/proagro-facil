c54a7a1e4a1f8eac1533c9df44cc682a147d7ae6
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-07 15:50:33 -0300

    feat: Add RegisterCard to Search component.

--- app/frontend/src/components/Search.jsx
+++ app/frontend/src/components/Search.jsx
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchBusca from '../services/fetches/fetchBusca';
+import validateId from '../services/validations/validateId';
+import RegisterCard from './RegisterCard';
 
-export default function Search() {
+export default function Delete() {
+  const [id, setId] = useState('');
+  const [enableBtn, setEnableBtn] = useState(false);
+  const [registerData, setRegisterData] = useState('');
+  const [showRegisterData, setShowRegisterData] = useState(false);
+  // const [message, setMessage] = useState('');
+  // const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (validateId(id)) {
+      setEnableBtn(true);
+    } else {
+      setEnableBtn(false);
+    }
+  }, [id]);
+
+  const handleSubmit = async () => {
+    const response = await fetchBusca(id);
+    if (response) {
+      setShowRegisterData(true);
+      setRegisterData(response);
+      setId('');
+    }
+  };
+
   return (
     <section className="w-full flex flex-col justify-center items-center ">
       <h2 className="page-title">Busca Cadastro</h2>
+      <div className="w-full max-w-lg mb-6 md:mb-0">
+        <label
+          className="
+            label-form"
+          htmlFor="form-name"
+        >
+          Id do Registro
+          <input
+            className="input-form"
+            id="form-name"
+            type="text"
+            placeholder="Id do Registro"
+            onChange={ ({ target }) => setId(() => target.value) }
+            value={ id }
+          />
+          {!enableBtn && (
+            <p className="text-red-500 text-xs  italic">
+              Adicione ID válido.
+            </p>)}
+          {enableBtn && (
+            <p className="text-green-800 text-xs italic">
+              ID válido.
+            </p>)}
+        </label>
+      </div>
+
+      <div className="w-full max-w-lg flex justify-center mt-3 mb-6 md:mb-0">
+        <button
+          className="w-1/3 button-form mx-auto"
+          type="button"
+          onClick={ handleSubmit }
+          disabled={ !enableBtn }
+        >
+          Buscar Registros
+        </button>
+      </div>
+      {showRegisterData && <RegisterCard registerData={ registerData } />}
     </section>
   );
 }
