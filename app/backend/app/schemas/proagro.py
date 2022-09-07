a28f5186bd7fe13d72fbfd92dd31a4d9b6b3baa3
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 22:04:03 -0300

    style: Format code of Schema.

--- app/backend/app/schemas/proagro.py
+++ app/backend/app/schemas/proagro.py
@@ -12,18 +12,19 @@ class RegisterSchema(BaseModel):
     event: str = Field(...)
 
     class Config:
-            schema_extra = {
-                "example": {  
-                    "name": "teste4",
-                    "email": "teste@teste.com",
-                    "cpf": "03214598678",
-                    "latitude": "56",
-                    "longitude": "55",
-                    "type": "milho",
-                    "date": "2022-10-25",
-                    "event": "granizo"
-                }
+        schema_extra = {
+            "example": {
+                "name": "teste4",
+                "email": "teste@teste.com",
+                "cpf": "03214598678",
+                "latitude": "56",
+                "longitude": "55",
+                "type": "milho",
+                "date": "2022-10-25",
+                "event": "granizo",
             }
+        }
+
 
 def ResponseModel(data, message):
     return {
