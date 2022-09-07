4ceb8f9df3125bc738643916ca19bb68ac3bc402
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-07 18:10:01 -0300

    fix: Fix coordenates validation and forms validation.

--- app/frontend/src/services/validations/validateCoordnates.js
+++ app/frontend/src/services/validations/validateCoordnates.js
@@ -1,5 +1,6 @@
 function validateCoordenate(coordenate) {
-  return !!(coordenate.match(/^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}/));
+  const matches = coordenate.match(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/g);
+  return (matches !== null && matches.length > 0);
 }
 
 export default validateCoordenate;
--- app/frontend/src/services/validations/validateForms.js
+++ app/frontend/src/services/validations/validateForms.js
@@ -7,9 +7,9 @@ function validateForms({ name, cpf, email, latitude, longitude }) {
   const minL = 6;
   if (name.length < minL) messages.push('O nome precisa ter mais de 6 caracteres.');
   if (!validateCPF(cpf)) messages.push('Forneça um cpf válido.');
-  if (EmailValidator.validate(email)) messages.push('Forneça email válido.');
-  if (validateCoordenate(latitude)) messages.push('Forneça latitude válida.');
-  if (validateCoordenate(longitude)) messages.push('Forneça longitude válida.');
+  if (!EmailValidator.validate(email)) messages.push('Forneça email válido.');
+  if (!validateCoordenate(latitude)) messages.push('Forneça latitude válida.');
+  if (!validateCoordenate(longitude)) messages.push('Forneça longitude válida.');
   console.log(messages);
   if (messages.length <= 0) return true;
   return messages;
