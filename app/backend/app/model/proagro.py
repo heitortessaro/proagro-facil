08c5135ab3bf7d526952ab24e31add9b3450d710
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-06 22:03:41 -0300

    feat: Add function to fetch_by_register and refactor other functions.
    The refactor of functions from the model were made to removed unused code.

--- app/backend/app/model/proagro.py
+++ app/backend/app/model/proagro.py
@@ -5,17 +5,18 @@ from typing import List
 
 # helpers
 def register_helper(register) -> dict:
-  return{
-      "id": str(register["_id"]),
-      "name": register["name"],
-      "email": register["email"],
-      "cpf": register["cpf"],
-      "latitude": register["latitude"],
-      "longitude": register["longitude"],
-      "type": register["type"],
-      "date": register["date"],
-      "event": register["event"],
-}
+    return {
+        "id": str(register["_id"]),
+        "name": register["name"],
+        "email": register["email"],
+        "cpf": register["cpf"],
+        "latitude": register["latitude"],
+        "longitude": register["longitude"],
+        "type": register["type"],
+        "date": register["date"],
+        "event": register["event"],
+    }
+
 
 # Add a new register into to the database
 async def add_register(register_data: dict) -> dict:
@@ -23,24 +24,36 @@ async def add_register(register_data: dict) -> dict:
     new_register = await proagro_collection.find_one({"_id": register.inserted_id})
     return register_helper(new_register)
 
+
 # find a register using id
 async def fetch_one_register(id: str) -> dict:
-  register = await proagro_collection.find_one({"_id": ObjectId(id)})
-  return register_helper(register)
+    register = await proagro_collection.find_one({"_id": ObjectId(id)})
+    return register_helper(register)
+
 
 # find registers using date
 async def fetch_registers_by_date(date: str) -> List[dict]:
-  registers = []
-  async for register in proagro_collection.find({"date": date}):
-    registers.append(register_helper(register))
-  return registers
+    registers = []
+    async for register in proagro_collection.find({"date": date}):
+        registers.append(register_helper(register))
+    return registers
+
+
+# find registers using date
+async def fetch_registers_by_cpf(cpf: str) -> List[dict]:
+    registers = []
+    async for register in proagro_collection.find({"cpf": cpf}):
+        registers.append(register_helper(register))
+    return registers
+
 
 # find all registers
 async def fetch_all() -> List[dict]:
-  registers = []
-  async for register in proagro_collection.find():
-    registers.append(register_helper(register))
-  return registers
+    registers = []
+    async for register in proagro_collection.find():
+        registers.append(register_helper(register))
+    return registers
+
 
 # delete a register
 async def delete_register(id: str):
@@ -50,17 +63,12 @@ async def delete_register(id: str):
         return True
     return False
 
+
 # update a register with matching id
 async def update_register(id: str, data: dict) -> dict:
-    # Return false if an empty request body is sent.
-    if len(data) < 1:
-        raise HTTPException(400, "Não foram encaminhados os dados atualizados do evento.")
-    register = await proagro_collection.find_one({"_id": ObjectId(id)})
-    if register:
-        updated_register = await proagro_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": data}
-        )
-        if updated_register:
-            return True
-        return False
+    updated_register = await proagro_collection.update_one(
+        {"_id": ObjectId(id)}, {"$set": data}
+    )
+    if updated_register:
+        return True
     return False
