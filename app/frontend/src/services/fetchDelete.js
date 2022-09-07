b5dabfa0677f58b86b704f1acbc47b69be44174b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-07 14:35:23 -0300

    feat: Defines fetchDelete to remove registers from the db.

--- app/frontend/src/services/fetchDelete.js
+++ app/frontend/src/services/fetchDelete.js
@@ -1,11 +1,17 @@
-const fetchBusca = async () => {
-  const url = '';
-  const response = await fetch(url);
-  const fetchOk = 200;
-  if (response.status === fetchOk) {
-    return { message: 'Registro apagado com sucesso.' };
+const fetchDelete = async (id) => {
+  const url = `http://0.0.0.0:8004/register/${id}`;
+  console.log(url);
+  const response = await fetch(url, { method: 'DELETE' });
+  console.log(response);
+  const successfullyRemoved = 204;
+  const notFound = 404;
+  if (response.status === successfullyRemoved) {
+    return 'Registro removido com sucesso';
+  } if (response.status === notFound) {
+    const data = await response.json();
+    return data.detail;
   }
-  return { message: 'falha na requisição' };
+  return false;
 };
 
-export default fetchBusca;
+export default fetchDelete;
